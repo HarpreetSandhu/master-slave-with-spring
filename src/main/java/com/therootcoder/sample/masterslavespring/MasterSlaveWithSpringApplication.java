878295fbd981c7261b1ba9e0c4100d1d208c9761
Harpreet Sandhu <happyenb@gmail.com>
2019-03-23 17:24:40 +0530

    - basic structure of project

---
+++ src/main/java/com/therootcoder/sample/masterslavespring/MasterSlaveWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.therootcoder.sample.masterslavespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSlaveWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSlaveWithSpringApplication.class, args);
+	}
+
+}
